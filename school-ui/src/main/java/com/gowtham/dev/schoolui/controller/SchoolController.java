06765fc0af0170dfaf8910706af78f2766ba5aa2
gowthamvarma <varma.dandu@ezypay.com>
2020-04-22 14:36:31 +1000

    feat(school-ui): added ui service
    - Adding new ui service and register school-ui to it which can be observed here: http://localhost:8080 after start
    
    #Closes [GD-03]

---
+++ school-ui/src/main/java/com/gowtham/dev/schoolui/controller/SchoolController.java
@@ -0,0 +1,40 @@
+package com.gowtham.dev.schoolui.controller;
+
+import com.gowtham.dev.schoolui.dto.TeachingClassDto;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class SchoolController {
+
+    private final RestTemplate restTemplate;
+
+    public SchoolController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @RequestMapping("")
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/classes")
+    @PreAuthorize("hasAuthority('SCOPE_profile')")
+    public ResponseEntity<List<TeachingClassDto>> listClasses() {
+
+        return restTemplate
+                .exchange("http://school-service/class", HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<TeachingClassDto>>() {
+                        });
+    }
+}
